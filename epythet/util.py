4725c242e3228c7e7088486e3f882ec743ffac64
twhalen <thor@otosense.com>
2020-09-25 16:25:21 -0700

    - mk_replacer_from_dict - mk_import_root_replacer

--- epythet/util.py
+++ epythet/util.py
@@ -1,5 +1,6 @@
 import re
-from typing import Mapping, Union, Iterable, Generator
+from itertools import chain
+from functools import partial
 
 
 def mk_conditional_logger(condition, func=print):
@@ -16,11 +17,16 @@ def clog(condition, func=print, *args, **kwargs):
 
 
 def mk_replacer_from_dict(from_to_dict):
-    """
+    """Make a function that does multiple replacements (in a single pass).
+
     >>> r = mk_replacer_from_dict({'is': 'are', 'life': 'butterflies'})
     >>> r("There is no life in the void.")
     'There are no butterflies in the void.'
+
+    :param from_to_dict: A dict of {to_find: to_replace_by,...} pairs
+    :return: A replacer function that you can apply to strings to carry out the replacements
     """
+
     p = re.compile('|'.join(map(r'({})'.format, map(re.escape, from_to_dict.keys()))))
     f = lambda x: from_to_dict[x.group(0)]
 
@@ -28,6 +34,137 @@ def mk_replacer_from_dict(from_to_dict):
         return p.sub(f, s)
 
     return replacer
+
+
+def mk_import_root_replacer(from_to_dict):
+    """Make a function that does multiple import name replacements.
+
+    For a use case, see replace_import_names. This is just a helper function.
+
+    >>> replace = mk_import_root_replacer({'foo': 'FOO', 'bar': 'BAR'})
+    >>>
+    >>> assert replace('from foo import BLAH') == 'from FOO import BLAH'
+    >>> assert replace('from foo.bar import BLAH') == 'from FOO.bar import BLAH'
+    >>> assert replace('import bar') == 'import BAR'
+
+    Partial matches are not replaced (that's a good thing!):
+
+    >>> assert replace('import barmitzvah as oy') == 'import barmitzvah as oy'
+    >>> assert replace('from foobar import hello') == 'from foobar import hello'
+
+    Yes, and it works with dotpaths:
+
+    >>> replace = mk_import_root_replacer({'where.it.was': 'where.it.is.now'})
+    >>> replace('import where.it.was as here')
+    'import where.it.is.now as here'
+
+    :param from_to_dict: A dict of {to_find: to_replace_by,...} pairs
+    :return: A replacer function that you can apply to strings to carry out the replacements
+
+    """
+    t = dict(chain.from_iterable(
+        [(f"(?<=from\ ){old}(?=[\.\ ])", f"{new}"), (f"(?<=import\ ){old}(?=[\.\s])", f"{new}")]
+        for old, new in from_to_dict.items()))
+
+    p = re.compile('|'.join(map(r'({})'.format, t.keys())))
+    f = lambda x: from_to_dict[x.group(0)]
+
+    def replacer(s):
+        ss = p.sub(f, s + ' ')
+        return ss[:-1]
+
+    return replacer
+
+
+def replace_import_names(
+        source_store,
+        from_to_dict,
+        target_store=None,
+        dryrun=True,
+        verbose=True,
+        replacer_factory=mk_import_root_replacer,
+        add_comment_at_the_end_of_lines_replaced=False,
+):
+    """Replace import names.
+
+    Use case: You've renamed something or moved some modules (remember UNIX? Same as move!) and have to go through
+    all your files and notebooks and replace those names.
+    Now, if you have a nice IDE, we suggest you use refactoring instead -- as long as you have any uses in the scope.
+    But sometimes it's not enough. You might have text/html documments, or jupyter notebooks, etc.
+    So you can use this instead.
+
+    Be warned though:
+    - You should look at the pattern that is used to match, and make sure it won't create havoc.
+    - Backup your documents so you can revert!
+    - Print matches before you actually apply them all in bulk.
+    - Use at your own risk!
+
+    For examples, see the mk_import_root_replacer helper function.
+
+    :param source_store:
+    :param from_to_dict: A dict of {to_find: to_replace_by,...} pairs
+    :param target_store: The store
+    :param dryrun:
+    :param verbose:
+    :param replacer_factory: makes the replacer = replacer_factory(from_to_dict)
+    :param add_comment_at_the_end_of_lines_replaced: True/False or an actual string to add at the end of replaced lines
+    :return:
+    """
+
+    suffix = ""
+    if add_comment_at_the_end_of_lines_replaced is False:
+        suffix = ""
+    elif add_comment_at_the_end_of_lines_replaced is True:
+        suffix = "# line_was_edited_by_epythet"
+    elif isinstance(add_comment_at_the_end_of_lines_replaced, str):
+        suffix = add_comment_at_the_end_of_lines_replaced
+        if not suffix.startswith('#'):
+            suffix = '# ' + suffix
+    else:
+        raise TypeError(f"Don't know what to do with such a type of add_comment_at_the_end_of_lines_replaced: "
+                        f"{add_comment_at_the_end_of_lines_replaced}")
+
+    _clog = partial(clog, condition=verbose)
+
+    if target_store is None:
+        target_store = {}  # use a dict to write results
+        clog("You didn't specify a target_store, so I'll write all of this in a dict and return it to you!")
+
+    if source_store == target_store:
+        # TODO: Add confirmation (user input) to protect more. Make this an option (so total automatic is possible)
+        _clog("I just wanted you to be aware that you are using the same store for source and target."
+              "This means I'm about to overwrite your files!!")
+        if dryrun:
+            _clog("... Right now, dryrun=True, so I'm just pretending!")
+
+    replacer = replacer_factory(from_to_dict)
+
+    if dryrun:
+        replace_prompt = 'will replace'
+    else:
+        replace_prompt = '   replacing'
+    with____prompt = '        with'
+
+    for k, v in source_store.items():
+
+        lines_replaced = 0
+
+        def gen():
+            nonlocal lines_replaced
+            for i, line in enumerate(v.split('\n'), 1):  # TODO: Double traversal. Find online splitter
+                new_line = replacer(line) + suffix
+                yield new_line
+                if new_line != line:  # TODO: Double traversal... could have replacer return a "replaced_something" flag
+                    lines_replaced += 1
+                    clog(verbose or dryrun, print,
+                         f"{k}:{i}:\n{replace_prompt}\t{line}\n{with____prompt}\t{new_line}")
+
+        new_v = '\n'.join(gen())
+
+        if not dryrun and lines_replaced > 0:
+            target_store[k] = new_v
+
+    return target_store
 
 
 fc = dict(
