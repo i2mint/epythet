7593a73f3157c48cc20ba9201bc2961c9dc42ee9
Andie Shining Phan <asphan@gmail.com>
2020-09-30 12:03:01 -0700

    update docs_gen docs

--- epythet/docs_gen/__init__.py
+++ epythet/docs_gen/__init__.py
@@ -21,6 +21,28 @@ Compile docs
     cd PROJECT_DIR/docsrc
     make html
 
+View by opening ``PROJECT_DIR/docsrc/_build/html/index.html``
+
+Github Pages
+------------
+
+Go to your repo settings and set GitHub Pages site to build from the ``/docs`` folder in the ``master`` branch.
+Github will tell you where it will be published.
+In this case, the site is published at https://i2mint.github.io/epythet/
+
+Compile docs for github
+::
+
+    cd PROJECT_DIR/docsrc
+    make github
+
+Push generated ``PROJECT_DIR/docs`` to ``master`` branch
+
+Editing and Customizing Docs
+----------------------------
+
+You can add RST documentation directly in the source files.
+`This source file for example </epythet/docs_gen/__init__.py>`_.
 """
 
 from pathlib import Path
