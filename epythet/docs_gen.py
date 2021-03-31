132f28e9ea0c312c8845ce32f3115846caa763f5
Thor Whalen <thorwhalen1@gmail.com>
2021-03-31 09:35:16 -0700

    added more documentation

--- epythet/docs_gen.py
+++ epythet/docs_gen.py
@@ -1,9 +1,4 @@
-import warnings
-
-warnings.warn(
-    "'from epythet.docs_gen import *' is deprecated. Use 'from epythet import *' instead",
-    DeprecationWarning,
-)
+"""Documentation generation"""
 
 from epythet.setup_docsrc import make_docsrc
 from epythet.autogen import make_autodocs
