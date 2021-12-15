77d70db3cc4f4f7e3d5b621128bb5e7455429914
Andie.Phan <Andie.Phan@analog.com>
2021-12-15 10:50:40 -0800

    add instructions for graphviz prerequisites

--- epythet/__init__.py
+++ epythet/__init__.py
@@ -23,6 +23,22 @@ Check your ``PROJECT_DIR/setup.cfg`` contains::
     copyright = 2020, Otosense
     display_name = Epythet
 
+For graphviz support:
+
+For MacOS::
+
+    brew install graphviz
+
+For Ubuntu::
+
+    sudo apt-get install graphviz
+
+For Windows::
+
+    Install windows package from: https://graphviz.gitlab.io/_pages/Download/Download_windows.html
+    Add C:\Program Files (x86)\Graphviz2.38\bin to User path
+    Add C:\Program Files (x86)\Graphviz2.38\bin\dot.exe to System Path
+
 3-in-1 Quickstart
 =================
 
