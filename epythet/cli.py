3dbcf20deb2819db63719703aa6d8928dc602a50
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-01-31 13:32:40 +0100

    feat: add ignore to quickstart

--- epythet/cli.py
+++ epythet/cli.py
@@ -3,6 +3,7 @@
 More info in the main epythet documentation page
 """
 
+from typing import List
 from pathlib import Path
 
 _STATIC_FILES = Path(__file__).absolute().parent / '_static'
@@ -11,8 +12,11 @@ from epythet.autogen import make_autodocs
 from epythet.setup_docsrc import make_docsrc
 from epythet.call_make import make
 
+import argh
 
-def quickstart(project_dir):
+
+@argh.arg('--ignore', nargs='*')
+def quickstart(project_dir, *, ignore: List[str] = None):
     """Quickstart will run through the three steps
 
     1. epythet make_docsrc project_dir
@@ -20,9 +24,10 @@ def quickstart(project_dir):
     3. epythet make project_dir html
 
     :param project_dir: Path to root project directory containing docsrc folder
+    :param ignore: skip file if path contains any ignore strings
     """
     make_docsrc(project_dir, verbose=True)
-    make_autodocs(project_dir, skip_existing=False)
+    make_autodocs(project_dir, skip_existing=False, ignore=ignore)
     make(project_dir, 'html')
 
 
